5f7d1df06b82b8e954f1a92553aa114be39965a8
xiaodongza <398186800@qq.com>
2021-12-18 22:58:35 +0800

    '12-18-2'

--- handle.go
+++ handle.go
@@ -88,6 +88,7 @@ func handleData() {
 			fmt.Println(*rec)
 		}
 	}
+
 }
 
 
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
 var dataPath *string
@@ -22,7 +23,7 @@ var dstPassword *string
 //
 //  you can test this example by:
 //  go run main.go --data_path /tmp/data --dst_ip 127.0.0.1 --dst_port 3306 --dst_user root --dst_password 123456789
-func m() {
+func main() {
 	dataPath = flag.String("data_path", "/tmp/data", "dir path of source data")
 	dstIP = flag.String("dst_ip", "", "ip of dst database address")
 	dstPort = flag.Int("dst_port", 0, "port of dst database address")
@@ -36,30 +37,34 @@ func m() {
 	fmt.Printf("dst user:%v\n", *dstUser)
 	fmt.Printf("dst password:%v\n", *dstPassword)
 	fmt.Println(*dataPath)
-	csvfile, err := os.Open(*dataPath + "/src_a/a/1.csv")
-	//fmt.Println(*dataPath + "src_a/a/1.csv")
-	if err != nil {
-		log.Fatalln("Couldn't open the csv file", err)
+	dbs := make([]string, 0)
+	dbs = append(dbs, "a", "b", "c", "d", "e", "f", "g")
+	for i := 1; i <= 28; i++ {
+		for _, db := range dbs {
+			csvfile, err := os.Open(*dataPath + "/src_a/" + db + "/" + strconv.Itoa(i) +".csv")
+			//fmt.Println(*dataPath + "src_a/a/1.csv")
+			if err != nil {
+				log.Fatalln("Couldn't open the csv file", err)
+			}
+			defer csvfile.Close()
+			// Parse the file
+			r := csv.NewReader(csvfile)
+			// Iterate through the records
+			for {
+				// Read each record from csv
+				record, err := r.Read()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+				for rec := range record {
+					log.Println("%s\n", rec)
+					fmt.Printf("%s\n", rec)
+				}
+			}
+		}
 	}
-	defer csvfile.Close()
-	// Parse the file
-	r := csv.NewReader(csvfile)
-	// Iterate through the records
-	for {
-		// Read each record from csv
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		//fmt.Printf("Record has %d columns.\n", len(record))
-		//city, _ := iconv.ConvertString(record[2], "gb2312", "utf-8")
-		//fmt.Printf("%s %s %s %s \n", record[0], record[1], record[2], record[3])
-		for rec := range record {
-			log.Println("%s\n", rec)
-			fmt.Printf("%s\n", rec)
-		}
-	}
+
 }
