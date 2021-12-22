3870fb10876eeada3a1e6b9bbe92324f4b19348e
xiaodongza <398186800@qq.com>
2021-12-22 13:00:36 +0800

    '12-22-3'

--- main.go
+++ main.go
@@ -34,7 +34,8 @@ import (
 	"sync/atomic"
 	"time"
 )
-type NullTime sql.NullTime
+
+//type NullTime sql.NullTime
 
 var collations = map[string]byte{
 	"big5_chinese_ci":      1,
@@ -3263,37 +3264,37 @@ func (mc *mysqlConn) handleInFileRequest(name string) (err error) {
 // Scan implements the Scanner interface.
 // The value type must be time.Time or string / []byte (formatted time-string),
 // otherwise Scan fails.
-func (nt *NullTime) Scan(value interface{}) (err error) {
-	if value == nil {
-		nt.Time, nt.Valid = time.Time{}, false
-		return
-	}
-
-	switch v := value.(type) {
-	case time.Time:
-		nt.Time, nt.Valid = v, true
-		return
-	case []byte:
-		nt.Time, err = parseDateTime(v, time.UTC)
-		nt.Valid = (err == nil)
-		return
-	case string:
-		nt.Time, err = parseDateTime([]byte(v), time.UTC)
-		nt.Valid = (err == nil)
-		return
-	}
-
-	nt.Valid = false
-	return fmt.Errorf("Can't convert %T to time.Time", value)
-}
-
-// Value implements the driver Valuer interface.
-func (nt NullTime) Value() (driver.Value, error) {
-	if !nt.Valid {
-		return nil, nil
-	}
-	return nt.Time, nil
-}
+//func (nt *NullTime) Scan(value interface{}) (err error) {
+//	if value == nil {
+//		nt.Time, nt.Valid = time.Time{}, false
+//		return
+//	}
+//
+//	switch v := value.(type) {
+//	case time.Time:
+//		nt.Time, nt.Valid = v, true
+//		return
+//	case []byte:
+//		nt.Time, err = parseDateTime(v, time.UTC)
+//		nt.Valid = (err == nil)
+//		return
+//	case string:
+//		nt.Time, err = parseDateTime([]byte(v), time.UTC)
+//		nt.Valid = (err == nil)
+//		return
+//	}
+//
+//	nt.Valid = false
+//	return fmt.Errorf("Can't convert %T to time.Time", value)
+//}
+//
+//// Value implements the driver Valuer interface.
+//func (nt NullTime) Value() (driver.Value, error) {
+//	if !nt.Valid {
+//		return nil, nil
+//	}
+//	return nt.Time, nil
+//}
 
 func (mc *mysqlConn) readPacket() ([]byte, error) {
 	var prevData []byte
